41961a1937f684a925fb4933d52a02f4cdb1a7e4
Shristi Kumari Verma <vkshristi@gmail.com>
2025-03-23 20:21:09 +0530

    Initial commit of Image applet project.

---
+++ ImageApplet/src/ImageApplet.java
@@ -0,0 +1,16 @@
+import java.applet.Applet;
+import java.awt.*;
+
+public class ImageApplet extends Applet {
+    Image img;  // Declare an image object
+
+    public void init() {
+        // Load the image from the given path
+        img = getImage(getDocumentBase(), "image.jpg");  // Image should be in the same directory
+    }
+
+    public void paint(Graphics g) {
+        // Draw the loaded image at position (50, 50)
+        g.drawImage(img, 50, 50, this);
+    }
+}
